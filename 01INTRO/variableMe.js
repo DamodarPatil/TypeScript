caacced544ae0a62e5414318ff80a49028f2305f
DamodarPatil <patildamodar1983@gmail.com>
2024-07-19 13:26:58 +0530

    initial commit

--- 01INTRO/variableMe.js
+++ 01INTRO/variableMe.js
@@ -1,4 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Damodar";
-console.log(greetings.toUpperCase());
+var myNum = 6;
+// myNum.toUpperCase()
+// greetings.toLowercase()
+greetings.toLowerCase();
+console.log(greetings);
+//number
+var userId = 123456.34;
+var userId2 = 45835.9;
+userId.toFixed();
+userId2.toFixed();
+//boolean
+var isLoggenIn = false;
+isLoggenIn.valueOf();
--- 01INTRO/variableMe.ts
+++ 01INTRO/variableMe.ts
@@ -1,12 +1,22 @@
 let greetings: string = "Hello Damodar";
-
-let myNum = 6
+let myNum = 6;
 
 // myNum.toUpperCase()
-
 // greetings.toLowercase()
-greetings.toLowerCase()
 
+greetings.toLowerCase();
 console.log(greetings);
+
+//number
+let userId: number = 123456.34;
+let userId2 = 45835.9;
+
+userId.toFixed();
+userId2.toFixed();
+
+//boolean
+let isLoggenIn: boolean = false;
+
+isLoggenIn.valueOf();
 
 export {};
