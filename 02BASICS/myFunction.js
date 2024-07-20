dfd81fba76e731666ed14996a8957aa22daf0a44
DamodarPatil <patildamodar1983@gmail.com>
2024-07-20 08:28:44 +0530

    initial commit

--- 02BASICS/myFunction.js
+++ 02BASICS/myFunction.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
     return num + 2;
+    // return "Damodar";
 }
 function getUpper(val) {
     return val.toUpperCase();
@@ -11,6 +12,7 @@ var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
 var myValue = addTwo(5);
+console.log(myValue);
 getUpper("Damodar");
 signUpUser("Damodar", "damodar@gmail.com", true);
 loginUser("Nitrohunter", "nitro@gmail.com");
--- 02BASICS/myFunction.ts
+++ 02BASICS/myFunction.ts
@@ -1,6 +1,6 @@
-function addTwo(num: number) {
-  // return num + 2;
-  return "Damodar"
+function addTwo(num: number): number {
+  return num + 2;
+  // return "Damodar";
 }
 
 function getUpper(val: string) {
@@ -11,11 +11,37 @@ function signUpUser(name: string, email: string, isPaid: boolean) {}
 
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
-let myValue= addTwo(5);
+let myValue = addTwo(5);
 
 getUpper("Damodar");
 
 signUpUser("Damodar", "damodar@gmail.com", true);
 
 loginUser("Nitrohunter", "nitro@gmail.com");
+
+// function getValue(myVal:number){
+//   if(myVal > 5){
+//     return true
+//   }
+//   return "200 OK"
+// }
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+const heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3];
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(err: string): void {
+  console.log(err);
+}
+
+function handleError(err: string): never {
+  throw new Error(err)
+}
 export {};
